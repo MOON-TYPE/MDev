563c5e3cf9b12217f1b220770b67c2a709634cf6
Moon Antonio <pinchodev91@gmail.com>
2017-05-10 04:22:19 +0200

    Add clase comando

---
+++ MDev Project/Assets/Moon Antonio/MDev/Scripts/Clases/Comando.cs
@@ -0,0 +1,30 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Comando.cs (10/05/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Clase de los comandos de MDev								\\
+// Fecha Mod:		10/05/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public class Comando 
+	{
+		#region Propiedades
+		private string nombreComando;
+		/// <summary>
+		/// <para>Nombre del comando</para>
+		/// </summary>
+		public string NombreComando
+		{
+			set { nombreComando = value; }
+			get {  return nombreComando; }
+		}
+		#endregion
+	}
+}
