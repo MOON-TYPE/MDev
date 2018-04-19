fa9eb44d8b26f1c505ff423e9dafa10d179943a4
Moon Antonio <pinchodev91@gmail.com>
2018-04-19 03:10:45 +0200

    Add input

--- MDev Project/Assets/Moon Antonio/MDev/Scripts/Core/Clases/MDevInput.cs
+++ MDev Project/Assets/Moon Antonio/MDev/Scripts/Core/Clases/MDevInput.cs
@@ -9,12 +9,115 @@
 
 #region Librerias
 using UnityEngine;
+using System;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.MDev
 {
-	public class MDevInput : MonoBehaviour 
+	/// <summary>
+	/// <para></para>
+	/// </summary>
+	public class MDevInput
 	{
+		#region Variables Privadas
+		private MDev mdev;
+		private float touchDelay = 1.5f;
+		private float tiempo = 0f;
+		#endregion
 
+		#region Constructor
+		public MDevInput(MDev instance)
+		{
+			mdev = instance;
+		}
+		#endregion
+
+		#region Actualizador
+		public void Update()
+		{
+			if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+			{
+				if (MobileInput())
+				{
+					mdev.ToggleMDev();
+					return;
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.LeftAlt))
+			{
+				mdev.ToggleMDev();
+				return;
+			}
+
+			if (!mdev.MostrarMDev) return;
+			if (Input.GetKeyDown(KeyCode.Backspace))
+			{
+				mdev.OnBackSpacePresionado();
+				return;
+			}
+
+			if (Input.GetKeyDown(KeyCode.Tab))
+			{
+				mdev.OnTabPresionado();
+				return;
+			}
+
+			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+			{
+				mdev.OnEnterPresionado();
+				return;
+			}
+
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				mdev.OnDownArrowPresionado();
+			}
+			else if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				mdev.OnUpArrowPresionado();
+			}	
+			else
+			{
+				if (Application.platform != RuntimePlatform.Android && Application.platform != RuntimePlatform.IPhonePlayer) mdev.UpdateInputText(Input.inputString);
+			}
+		}
+		#endregion
+
+		#region Funcionalidad
+		private bool MobileInput()
+		{
+			// Si hay teclado
+			if (mdev.touchScreenKeyboard != null)
+			{
+				if (mdev.touchScreenKeyboard.done)
+				{
+					// Establecer el texto
+					mdev.SetInputText(mdev.touchScreenKeyboard.text);
+					mdev.OnEnterPresionado();
+					mdev.touchScreenKeyboard = null;
+				}
+			}
+
+			// Comprobar si se ha tocado la pantalla lo suficiente
+			if (Input.touchCount == mdev.data.mobileTouchCount)
+			{
+				tiempo += Time.deltaTime;
+			}
+
+			// Mostrar MDev y resetear tiempo
+			if (tiempo > touchDelay)
+			{
+				tiempo = 0;
+				if (mdev.MostrarMDev)
+				{
+					mdev.MostrarTouchScreenKeyboard();
+					return false;
+				}
+				return true;
+			}
+			else return false;
+		}
+		#endregion
 	}
 }
--- MDev Project/Assets/Moon Antonio/MDev/Scripts/Core/Sistema/MDev.cs
+++ MDev Project/Assets/Moon Antonio/MDev/Scripts/Core/Sistema/MDev.cs
@@ -46,12 +46,87 @@ namespace MoonAntonio.MDev
 			Historial += MDevLinea + InputText + "\n" + (!string.IsNullOrEmpty(result) ? (result + "\n") : "");
 			InputText = "";*/
 		}
+
+		public void MostrarTouchScreenKeyboard()
+		{
+			if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+			{
+				if (MostrarMDev)
+				{
+					touchScreenKeyboard = TouchScreenKeyboard.Open(InputText, TouchScreenKeyboardType.Default);
+					TouchScreenKeyboard.hideInput = true;
+				}
+			}
+		}
+
+		/// <summary>
+		/// <para>Para teclados de PC.</para>
+		/// </summary>
+		/// <param name="input"></param>
+		public void UpdateInputText(string input)
+		{
+			InputText += input;
+			InputText = InputText.Replace("\b", "");
+		}
 		#endregion
 
 		#region Metodos Internos
+		internal void ToggleMDev()
+		{
+			MostrarMDev = !MostrarMDev;
+			MostrarTouchScreenKeyboard();
+		}
+
 		internal void CambiarInput(string input)
 		{
 			InputText = input;
+		}
+
+		/// <summary>
+		/// <para>Para teclados de android.</para>
+		/// </summary>
+		/// <param name="inputString"></param>
+		internal void SetInputText(string input)
+		{
+			InputText = input;
+		}
+		#endregion
+
+		#region Eventos
+		internal void OnBackSpacePresionado()
+		{
+			if (InputText.Length >= 1) InputText = InputText.Substring(0, InputText.Length - 1);
+		}
+
+		internal void OnEnterPresionado()
+		{
+			if (AutoCompletar.Count > 0)
+			{
+				InputText = AutoCompletar[AutoCompletarIndex];
+				AutoCompletar.Clear();
+			}
+			else
+			{
+				PreEjecutar();
+			}		
+		}
+
+		internal void OnTabPresionado()
+		{
+			if (AutoCompletar.Count != 0) { OnEnterPresionado(); return; }
+			AutoCompletarIndex = 0;
+			AutoCompletar.Clear();
+			AutoCompletar.AddRange(mdevMetodos.GetComandos(InputText));
+		}
+
+		internal void OnUpArrowPresionado()
+		{
+			if (AutoCompletar.Count > 0) AutoCompletarIndex = (int)Mathf.Repeat(AutoCompletarIndex - 1, AutoCompletar.Count);
+		}
+
+		internal void OnDownArrowPresionado()
+		{
+			if (AutoCompletar.Count > 0) AutoCompletarIndex = (int)Mathf.Repeat(AutoCompletarIndex + 1, AutoCompletar.Count);
 		}
 		#endregion
 	}
